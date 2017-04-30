5c863e25fd95d83cf5021c899f6e0bf5929d54c0
ikvir@mobilefringe.com @ CodeCloud Staging <ikvir@mobilefringe.com @ CodeCloud Staging>
2017-04-29 23:01:20 -0400

    Vue.js 1.0  /notfoundcomponent.vue

---
+++ notfoundcomponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{text}}
+  </div>
+</template>
+
+<script>
+  define(["Vue"], function(Vue) {
+    return Vue.component("not-found-component", {
+      template: template, // the variable template will be injected 
+      data: function() {
+        return {"text": "404 not found"};
+      }
+    });
+  });
+</script>
