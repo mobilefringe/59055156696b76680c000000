7e0f0147ace4d12ad8158e02f2498a5cccac883c
ikvir@mobilefringe.com @ CodeCloud Staging <ikvir@mobilefringe.com @ CodeCloud Staging>
2017-05-01 13:03:28 -0400

    Single Page Application with Vue.js 1.0  /notfoundcomponent.vue

--- notfoundcomponent.vue
+++ notfoundcomponent.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-    {{text}}
+  <div class="row">
+    <div class="large-6 columns">
+      <h1>{{text}}</h1>
+    </div>
   </div>
 </template>
 
 <script>
   define(["Vue"], function(Vue) {
     return Vue.component("not-found-component", {
-      template: template, // the variable template will be injected 
+      template: template, // the variable template will be injected
       data: function() {
-        return {"text": "404 not found"};
+        return {"text": "Custom 404 Page"};
       }
     });
   });
