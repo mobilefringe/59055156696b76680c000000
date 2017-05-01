36ba307ec21ea5c5289190150606344592ee18ef
ikvir@mobilefringe.com @ CodeCloud Staging <ikvir@mobilefringe.com @ CodeCloud Staging>
2017-05-01 10:59:30 -0400

    Single Page Application with Vue.js 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -2,10 +2,12 @@
   <div class="container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
     <div class="columns large-3 medium-6" v-for="store in stores">
       <div class="card">
-        <div class="card-divider" v-bind="store.name"></div>
+        <div class="card-divider">
+          {{ store.name }}
+        </div>
         <a :href="store.image_url" target="_blank"><img :src="store.image_url"></a>
         <div class="card-section">
-          <p v-bind="store.description"></p>
+          <p>{{ store.description }}</p>
         </div>
       </div>
     </div>
